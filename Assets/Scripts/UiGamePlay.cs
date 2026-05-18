4391e0ef8de1d21017926d7671ef81041e4b39de
4rleco <117652001+4rleco@users.noreply.github.com>
2026-05-18 17:28:11 -0300

    Add a pause script for the gameplay ui

---
+++ Assets/Scripts/UiGamePlay.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class UiGamePlay : MonoBehaviour
+{
+    [SerializeField] private GameObject panelSettings;
+
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.P) ||
+            Input.GetKey(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+        panelSettings.SetActive(isPaused);
+        Cursor.lockState = isPaused ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = isPaused;
+    }
+}
