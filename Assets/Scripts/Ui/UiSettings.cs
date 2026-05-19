539434a07a0d49161dfa2deff1e98a36879c4dad
4rleco <117652001+4rleco@users.noreply.github.com>
2026-05-18 23:34:16 -0300

    Add ui settings script

---
+++ Assets/Scripts/Ui/UiSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiSettings : MonoBehaviour
+{
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+}
